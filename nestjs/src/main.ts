6dd04fb9755ff39c6a89b76423f293332b3df541
Fernando Rocha <fernandorochaworld@gmail.com>
2025-02-06 20:58:10 -0800

    Update: setup ValidationPipe

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 const appPort = 4500;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
   await app.listen(appPort);
 }
 bootstrap();
