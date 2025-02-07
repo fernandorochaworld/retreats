482ed9470c5bedd87b5e080493ad5532e2187eba
Fernando Rocha <fernandorochaworld@gmail.com>
2025-02-06 20:59:21 -0800

    Update: Implemented app controller and service.

--- nestjs/src/app.controller.ts
+++ nestjs/src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, HttpCode, HttpStatus, Query } from '@nestjs/common';
+import { AppService, Venue } from './app.service';
+import { VenueDto } from './dto/venue.dto/venue.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getVenues(): string {
-    return this.appService.getVenues();
+  @HttpCode(HttpStatus.OK)
+  getVenues(@Query() query: VenueDto): Promise<Venue[]> {
+    return this.appService.getVenues(query.limit);
   }
 }
--- nestjs/src/app.service.ts
+++ nestjs/src/app.service.ts
@@ -1,20 +1,22 @@
 import { Injectable } from '@nestjs/common';
 
 // ! You'll need this when querying the database
-// import knexInstance from '../knex/knex';
+import knexInstance from '../knex/knex';
 
 // ! Left here for ease
-// export interface Venue {
-//   id: number;
-//   name: string;
-//   country_iso2: string;
-//   state: string;
-//   city: string;
-// }
+export interface Venue {
+  id: number;
+  name: string;
+  country_iso2: string;
+  state: string;
+  city: string;
+}
+
+const DEFAULT_LIMIT_INDEX_VENUE = 10;
 
 @Injectable()
 export class AppService {
-  getVenues(): string {
-    return 'Hello Venues!';
+  async getVenues(limit: number = DEFAULT_LIMIT_INDEX_VENUE): Promise<Venue[]> {
+    return await knexInstance<Venue>('venues').limit(limit);
   }
 }
