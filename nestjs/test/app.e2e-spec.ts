af37adcb119105228b12459136302be905e4bf37
Fernando Rocha <fernandorochaworld@gmail.com>
2025-02-06 21:01:46 -0800

    Update: Added e2e test.

--- nestjs/test/app.e2e-spec.ts
+++ nestjs/test/app.e2e-spec.ts
@@ -1,24 +1,82 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
+import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
+import { AppService, Venue } from '../src/app.service';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
+  const venues: Venue[] = [
+    {
+      id: 1,
+      name: 'Venue 1',
+      country_iso2: 'US',
+      state: 'CA',
+      city: 'Los Angeles',
+    },
+    {
+      id: 2,
+      name: 'Venue 2',
+      country_iso2: 'US',
+      state: 'CA',
+      city: 'San Francisco',
+    },
+  ];
+  let app: INestApplication<App>;
+  const appService: AppService = {
+    getVenues: () => Promise.resolve(venues),
+  };
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+      // .overrideProvider(AppService)
+      // .useValue(appService)
+      .compile();
 
     app = moduleFixture.createNestApplication();
+    // Register ValidationPipe globally to handle exceptions properly.
+    app.useGlobalPipes(new ValidationPipe({ transform: true }));
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/ (GET) No Argument', () => {
+    return (
+      request(app.getHttpServer())
+        .get('/')
+        .expect(200)
+        // .expect(venues)
+        .then((response) => {
+          expect(response.body).toHaveLength(10);
+        })
+    );
+  });
+
+  it('/ (GET) Limit 2', () => {
+    return (
+      request(app.getHttpServer())
+        .get('/?limit=2')
+        .expect(200)
+        // .expect(venues)
+        .then((response) => {
+          expect(response.body).toHaveLength(2);
+        })
+    );
+  });
+
+  it('/ (GET) Invalid Limit', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .get('/?limit=abcd')
+      .expect(400)
+      .expect({
+        message: ['limit must be an integer number'],
+        error: 'Bad Request',
+        statusCode: 400,
+      });
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
